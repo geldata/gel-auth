scotttrinh/fast-jelly

Implement EdgeDB+FastAPI guide application
Almost the whole thing. Missing some of the event domain for now.

app/queries/create_event_async_edgeql.py
@@ -1,0 +1,79 @@
+# AUTOGENERATED FROM 'app/queries/create_event.edgeql' WITH:
+#     $ edgedb-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import edgedb
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class CreateEventResult(NoPydanticValidation):
+    host: CreateEventResultHost | None
+    schedule: datetime.datetime | None
+    name: Str50
+    address: str | None
+    id: uuid.UUID
+    created_at: datetime.datetime
+
+
+@dataclasses.dataclass
+class CreateEventResultHost(NoPydanticValidation):
+    created_at: datetime.datetime
+    id: uuid.UUID
+    name: Str50
+
+
+async def create_event(
+    executor: edgedb.AsyncIOExecutor,
+    *,
+    name: str,
+    address: str,
+    schedule: datetime.datetime,
+    host_name: str,
+) -> CreateEventResult:
+    return await executor.query_single(
+        """\
+        with
+            name := <str>$name,
+            address := <str>$address,
+            schedule := <datetime>$schedule,
+            host_name := <str>$host_name,
+
+            HOST := (select default::User filter .name = host_name),
+            CREATED := (
+                insert default::Event {
+                    name := name,
+                    address := address,
+                    schedule := schedule,
+                    host := HOST,
+                }
+            ),
+        select CREATED { *, host: { * } };\
+        """,
+        name=name,
+        address=address,
+        schedule=schedule,
+        host_name=host_name,
+    )

app/queries/create_user_async_edgeql.py
@@ -1,0 +1,55 @@
+# AUTOGENERATED FROM 'app/queries/create_user.edgeql' WITH:
+#     $ edgedb-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import edgedb
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class CreateUserResult(NoPydanticValidation):
+    name: Str50
+    id: uuid.UUID
+    created_at: datetime.datetime
+
+
+async def create_user(
+    executor: edgedb.AsyncIOExecutor,
+    *,
+    name: str,
+) -> CreateUserResult:
+    return await executor.query_single(
+        """\
+        with
+            name := <str>$name,
+            NEW_USER := (
+              insert default::User {
+                name := name,
+              }
+            ),
+        select NEW_USER { * };\
+        """,
+        name=name,
+    )

app/queries/delete_user_async_edgeql.py
@@ -1,0 +1,53 @@
+# AUTOGENERATED FROM 'app/queries/delete_user.edgeql' WITH:
+#     $ edgedb-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import edgedb
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class DeleteUserResult(NoPydanticValidation):
+    name: Str50
+    id: uuid.UUID
+    created_at: datetime.datetime
+
+
+async def delete_user(
+    executor: edgedb.AsyncIOExecutor,
+    *,
+    name: str,
+) -> DeleteUserResult | None:
+    return await executor.query_single(
+        """\
+        with
+            name := <str>$name,
+            DELETED := (
+              delete default::User filter .name = name
+            )
+        select DELETED { * };\
+        """,
+        name=name,
+    )

app/queries/get_user_by_name_async_edgeql.py
@@ -1,0 +1,51 @@
+# AUTOGENERATED FROM 'app/queries/get_user_by_name.edgeql' WITH:
+#     $ edgedb-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import edgedb
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class GetUserByNameResult(NoPydanticValidation):
+    created_at: datetime.datetime
+    id: uuid.UUID
+    name: Str50
+
+
+async def get_user_by_name(
+    executor: edgedb.AsyncIOExecutor,
+    *,
+    name: str,
+) -> GetUserByNameResult | None:
+    return await executor.query_single(
+        """\
+        with
+            name := <str>$name,
+        select default::User { * }
+        filter .name = name;\
+        """,
+        name=name,
+    )

app/queries/get_users_async_edgeql.py
@@ -1,0 +1,45 @@
+# AUTOGENERATED FROM 'app/queries/get_users.edgeql' WITH:
+#     $ edgedb-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import edgedb
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class GetUsersResult(NoPydanticValidation):
+    created_at: datetime.datetime
+    id: uuid.UUID
+    name: Str50
+
+
+async def get_users(
+    executor: edgedb.AsyncIOExecutor,
+) -> list[GetUsersResult]:
+    return await executor.query(
+        """\
+        select default::User { * };\
+        """,
+    )

app/queries/update_user_async_edgeql.py
@@ -1,0 +1,60 @@
+# AUTOGENERATED FROM 'app/queries/update_user.edgeql' WITH:
+#     $ edgedb-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import edgedb
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class UpdateUserResult(NoPydanticValidation):
+    created_at: datetime.datetime
+    id: uuid.UUID
+    name: Str50
+
+
+async def update_user(
+    executor: edgedb.AsyncIOExecutor,
+    *,
+    current_name: str,
+    new_name: str,
+) -> UpdateUserResult | None:
+    return await executor.query_single(
+        """\
+        with
+            current_name := <str>$current_name,
+            new_name := <str>$new_name,
+            USER := (select default::User filter .name = current_name),
+            UPDATED := (
+                update USER
+                set {
+                    name := new_name,
+                }
+            ),
+        select UPDATED { * };\
+        """,
+        current_name=current_name,
+        new_name=new_name,
+    )

geldata/gel-auth

python-fastapi/examples/basic-async/app/queries/create_event_async_edgeql.py
@@ -1,0 +1,79 @@
+# AUTOGENERATED FROM 'app/queries/create_event.edgeql' WITH:
+#     $ gel-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import gel
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class CreateEventResult(NoPydanticValidation):
+    host: CreateEventResultHost | None
+    schedule: datetime.datetime | None
+    name: Str50
+    address: str | None
+    id: uuid.UUID
+    created_at: datetime.datetime
+
+
+@dataclasses.dataclass
+class CreateEventResultHost(NoPydanticValidation):
+    created_at: datetime.datetime
+    id: uuid.UUID
+    name: Str50
+
+
+async def create_event(
+    executor: gel.AsyncIOExecutor,
+    *,
+    name: str,
+    address: str,
+    schedule: datetime.datetime,
+    host_name: str,
+) -> CreateEventResult:
+    return await executor.query_single(
+        """\
+        with
+            name := <str>$name,
+            address := <str>$address,
+            schedule := <datetime>$schedule,
+            host_name := <str>$host_name,
+
+            HOST := (select default::User filter .name = host_name),
+            CREATED := (
+                insert default::Event {
+                    name := name,
+                    address := address,
+                    schedule := schedule,
+                    host := HOST,
+                }
+            ),
+        select CREATED { *, host: { * } };\
+        """,
+        name=name,
+        address=address,
+        schedule=schedule,
+        host_name=host_name,
+    )

python-fastapi/examples/basic-async/app/queries/create_user_async_edgeql.py
@@ -1,0 +1,55 @@
+# AUTOGENERATED FROM 'app/queries/create_user.edgeql' WITH:
+#     $ gel-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import gel
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class CreateUserResult(NoPydanticValidation):
+    name: Str50
+    id: uuid.UUID
+    created_at: datetime.datetime
+
+
+async def create_user(
+    executor: gel.AsyncIOExecutor,
+    *,
+    name: str,
+) -> CreateUserResult:
+    return await executor.query_single(
+        """\
+        with
+            name := <str>$name,
+            NEW_USER := (
+              insert default::User {
+                name := name,
+              }
+            ),
+        select NEW_USER { * };\
+        """,
+        name=name,
+    )

python-fastapi/examples/basic-async/app/queries/delete_user_async_edgeql.py
@@ -1,0 +1,53 @@
+# AUTOGENERATED FROM 'app/queries/delete_user.edgeql' WITH:
+#     $ gel-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import gel
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class DeleteUserResult(NoPydanticValidation):
+    name: Str50
+    id: uuid.UUID
+    created_at: datetime.datetime
+
+
+async def delete_user(
+    executor: gel.AsyncIOExecutor,
+    *,
+    name: str,
+) -> DeleteUserResult | None:
+    return await executor.query_single(
+        """\
+        with
+            name := <str>$name,
+            DELETED := (
+              delete default::User filter .name = name
+            )
+        select DELETED { * };\
+        """,
+        name=name,
+    )

python-fastapi/examples/basic-async/app/queries/get_user_by_name_async_edgeql.py
@@ -1,0 +1,51 @@
+# AUTOGENERATED FROM 'app/queries/get_user_by_name.edgeql' WITH:
+#     $ gel-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import gel
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class GetUserByNameResult(NoPydanticValidation):
+    created_at: datetime.datetime
+    id: uuid.UUID
+    name: Str50
+
+
+async def get_user_by_name(
+    executor: gel.AsyncIOExecutor,
+    *,
+    name: str,
+) -> GetUserByNameResult | None:
+    return await executor.query_single(
+        """\
+        with
+            name := <str>$name,
+        select default::User { * }
+        filter .name = name;\
+        """,
+        name=name,
+    )

python-fastapi/examples/basic-async/app/queries/get_users_async_edgeql.py
@@ -1,0 +1,45 @@
+# AUTOGENERATED FROM 'app/queries/get_users.edgeql' WITH:
+#     $ gel-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import gel
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class GetUsersResult(NoPydanticValidation):
+    created_at: datetime.datetime
+    id: uuid.UUID
+    name: Str50
+
+
+async def get_users(
+    executor: gel.AsyncIOExecutor,
+) -> list[GetUsersResult]:
+    return await executor.query(
+        """\
+        select default::User { * };\
+        """,
+    )

python-fastapi/examples/basic-async/app/queries/update_user_async_edgeql.py
@@ -1,0 +1,60 @@
+# AUTOGENERATED FROM 'app/queries/update_user.edgeql' WITH:
+#     $ gel-py
+
+
+from __future__ import annotations
+import dataclasses
+import datetime
+import gel
+import uuid
+
+
+Str50 = str
+
+
+class NoPydanticValidation:
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type, _handler):
+        # Pydantic 2.x
+        from pydantic_core.core_schema import any_schema
+        return any_schema()
+
+    @classmethod
+    def __get_validators__(cls):
+        # Pydantic 1.x
+        from pydantic.dataclasses import dataclass as pydantic_dataclass
+        _ = pydantic_dataclass(cls)
+        cls.__pydantic_model__.__get_validators__ = lambda: []
+        return []
+
+
+@dataclasses.dataclass
+class UpdateUserResult(NoPydanticValidation):
+    created_at: datetime.datetime
+    id: uuid.UUID
+    name: Str50
+
+
+async def update_user(
+    executor: gel.AsyncIOExecutor,
+    *,
+    current_name: str,
+    new_name: str,
+) -> UpdateUserResult | None:
+    return await executor.query_single(
+        """\
+        with
+            current_name := <str>$current_name,
+            new_name := <str>$new_name,
+            USER := (select default::User filter .name = current_name),
+            UPDATED := (
+                update USER
+                set {
+                    name := new_name,
+                }
+            ),
+        select UPDATED { * };\
+        """,
+        current_name=current_name,
+        new_name=new_name,
+    )